5c8e71f08085cdfadad4869a74f45798486e4bee
jiahc <jhc_32@163.com>
2015-01-22 21:15:22 +0800

    update

--- graph-save/src/com/pic/util/ImageUtil.java
+++ graph-save/src/com/pic/util/ImageUtil.java
@@ -5,20 +5,27 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
 import com.pic.model.ImageInfo;
 
 public class ImageUtil {
-	
-	 // 根据map对象包含的图片信息重建图片
+
+    /**
+     * 根据image对象包含的图片信息重建图片
+     * @param image
+     * @param outPath 图片生成的绝对路径 （文件夹）
+     * @return
+     */
     public static boolean createImage(ImageInfo image, String outPath) {
         if (image == null) {
             System.out.println("map 输入为 null");
             return false;
         }
         OutputStream output = null;
+        String fileName = image.getFileName();
         int imageType = image.getImageType();
         int width = image.getWidth();
         int height = image.getHeight();
@@ -28,7 +35,7 @@ public class ImageUtil {
         int[] pixelArray = image.getPixelArray();
         String formatType = image.getFormatType();
         try {
-            File file = new File(outPath);
+            File file = new File(outPath+"\\"+fileName);
             if (!file.exists()) {
                 File dir = file.getParentFile();
                 if (!dir.exists()) dir.mkdirs();
@@ -51,11 +58,67 @@ public class ImageUtil {
             if (output != null) try {
                 output.close();
             } catch (IOException e) {
-            	
+
             }
         }
         return true;
     }
+
+    /**
+     * 批量重建图像
+     * @param imageList
+     * @param folder  在项目主目录下生成该文件夹
+     * @return
+     */
+    public static boolean createImageBatch(List<ImageInfo> imageList, String folder) {
+        if (imageList == null) {
+            System.out.println("imageList 输入为 null");
+            return false;
+        }
+        OutputStream output = null;
+        try {
+            for (ImageInfo image : imageList) {
+                String fileName = image.getFileName();
+                int imageType = image.getImageType();
+                int width = image.getWidth();
+                int height = image.getHeight();
+                int scansize = width;
+                int minx = image.getMinx();
+                int miny = image.getMiny();
+                int[] pixelArray = image.getPixelArray();
+                String formatType = image.getFormatType();
+
+                File file =
+                        new File(System.getProperty("user.dir") + "\\" + folder + "\\" + fileName);
+                if (!file.exists()) {
+                    File dir = file.getParentFile();
+                    if (!dir.exists()) dir.mkdirs();
+                    file.createNewFile();
+                } else {
+                    System.out.println("图片已存在");
+                    return false;
+                }
+
+                output = new FileOutputStream(file);
+                BufferedImage imgOut = new BufferedImage(width, height, imageType);
+                // offset 暂时设定为0
+                // imgOut.setRGB(minx, miny, width, height, pixelArray, offset, scansize);
+                imgOut.setRGB(minx, miny, width, height, pixelArray, 0, scansize);
+                ImageIO.write(imgOut, formatType, output);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (output != null) try {
+                output.close();
+            } catch (IOException e) {
+
+            }
+        }
+        return true;
+    }
+
 
 
 }
