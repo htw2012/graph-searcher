66668449971fbf69c4dcbbe525bd1ed86fbde85e
towan <tongwenzide@163.com>
2015-05-10 15:54:02 +0800

    add volatile

--- towan-java-javatunning/src/main/java/com/buaa/edu/concurrent/method/volatitle/VolatileModel.java
+++ towan-java-javatunning/src/main/java/com/buaa/edu/concurrent/method/volatitle/VolatileModel.java
@@ -4,11 +4,12 @@ import org.junit.Test;
 //使用-server参数运行 FIXME
 public class VolatileModel {
 	 public class MyThread extends Thread{
-		 private   boolean stop = false;  
+	     //使用volatile确保stop变量在多线程中可见
+		 private volatile  boolean stop = false;  
 		 public void stopMe(){
 			 stop=true;
 		 }
-		 
+		 @Override
          public void run() {  
              int i = 0;  
              while (!stop) { 
@@ -25,5 +26,6 @@ public class VolatileModel {
 		 Thread.sleep(1000);
 		 t.stopMe();
 		 Thread.sleep(1000);
+		 
 	 }
 }
--- towan-java-javatunning/src/main/java/com/buaa/edu/concurrent/method/volatitle/VolatileTest.java
+++ towan-java-javatunning/src/main/java/com/buaa/edu/concurrent/method/volatitle/VolatileTest.java
@@ -3,13 +3,16 @@ package com.buaa.edu.concurrent.method.volatitle;
 import org.junit.Test;
 
 public class VolatileTest {
-	
+	//确保该变量在多线程可见
     volatile boolean isExit;
 
 	public void tryExit() {
-	  //成立则退出 FIXME 思考这样的情况
-		if (isExit == !isExit)
-			System.exit(0);
+	  //swap线程对isExit修改很快的被tryExit发现，因为它设法向主内存区中获取数据，当它获取等式左边、右边的数据前swap线程可能已经把数据修改了
+		if (isExit == !isExit){
+		    System.out.println("Swap thread has changed value");
+		    System.exit(0);
+		}
+		
 	}
 
 	public void swapValue() {
@@ -19,6 +22,7 @@ public class VolatileTest {
 	@Test
 	public void test() throws InterruptedException {
 		final VolatileTest volObj = new VolatileTest();
+		//线程1
 		Thread mainThread = new Thread() {
 			public void run() {
 				System.out.println("mainThread start");
@@ -30,6 +34,7 @@ public class VolatileTest {
 		};
 		mainThread.start();
 		
+		//线程2
 		Thread swapThread = new Thread() {
 			public void run() {
 				System.out.println("swapThread start");
@@ -39,6 +44,7 @@ public class VolatileTest {
 			}
 		};
 		swapThread.start();
+		
 		Thread.sleep(10000);
 	}
 }
